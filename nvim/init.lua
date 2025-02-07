dd60f10eb9fc994f729eb969247b83052417bb29
Ashley Fae <ashley.fae.uwu@gmail.com>
2025-02-07 17:22:26 +0100

    🚧 Switched to a multifile config, changed some NeoVim options

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+require("options")
+
 -- Function to get the today's coding time using WakaTime
 local uv = require("luv")
 
@@ -21,7 +23,7 @@ local function update_wakatime()
     local stderr = uv.new_pipe()
 
     local handle, pid =
-        uv.spawn(
+    uv.spawn(
         "/home/ashley/.wakatime/wakatime-cli",
         {
             args = {"--today"},
@@ -71,27 +73,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 
--- Vim settings
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
-vim.opt.wrap = false
-vim.opt.laststatus = 3
-vim.opt.termguicolors = true
-vim.diagnostic.config({
-	update_in_insert = true,
-	virtual_text = false,
-	virtual_lines = {
-		current_line = true
-	},
-	signs = true,
-	underline = true
-})
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -544,11 +526,11 @@ require("lazy").setup({
 		{
 			"wakatime/vim-wakatime",
 			lazy = false
-		}
+		},
 	},
 
 	-- Automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = false },
 })
 
 -- Change all diagnostic highlights to use curly lines
@@ -568,3 +550,100 @@ vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", {
 	undercurl = true,
 	sp = vim.api.nvim_get_hl_by_name("DiagnosticHint", true).foreground
 });
+
+--------------------------------------------------------------------------------
+-- GENERAL KEYMAPS
+--------------------------------------------------------------------------------
+-- For convenience
+local keymap = vim.keymap.set
+
+-- Remap COQ's <C-h> for cycling through placeholders to <Tab>
+vim.g.coq_settings = {
+	keymap = {
+		jump_to_mark = ""
+	}
+}
+
+-- Window navigation
+keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+-- Toggle the file explorer with <C-n>
+keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- Cycle buffers with Tab
+keymap("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
+keymap("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>x", ":bdelete<CR>", { noremap = true, silent = true })
+
+--------------------------------------------------------------------------------
+-- LSP-RELATED KEYMAPS
+--------------------------------------------------------------------------------
+-- The recommended way is to set them *only* when an LSP actually attaches.
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    -- event.buf = buffer number
+    local buf = event.buf
+
+    -- Common LSP actions
+    -- Go to definition
+    keymap("n", "gd", function()
+      vim.lsp.buf.definition()
+    end, { buffer = buf, desc = "Go to Definition" })
+
+    -- Show LSP hover doc
+    keymap("n", "K", function()
+      vim.lsp.buf.hover()
+    end, { buffer = buf, desc = "Hover Documentation" })
+
+    -- Go to references
+    keymap("n", "gr", function()
+      vim.lsp.buf.references()
+    end, { buffer = buf, desc = "Go to References" })
+
+    -- Show code actions
+    keymap({ "n", "v" }, "<leader>ca", function()
+      vim.lsp.buf.code_action()
+    end, { buffer = buf, desc = "Code Action" })
+
+    -- Rename symbol
+    keymap("n", "<leader>rn", function()
+      vim.lsp.buf.rename()
+    end, { buffer = buf, desc = "Rename Symbol" })
+
+    -- Diagnostics
+    keymap("n", "gl", function()
+      vim.diagnostic.open_float()
+    end, { buffer = buf, desc = "Line Diagnostics" })
+
+    -- etc. (You can add more as needed)
+  end
+})
+
+-- Only show virtual lines on keybind and hide it right after
+local function show_line_virtual_diagnostics()
+  -- Save the current diagnostic settings so we can revert
+  local old_config = vim.deepcopy(vim.diagnostic.config())
+
+  -- Turn on virtual_lines just for the current line
+  vim.diagnostic.config({
+    virtual_lines = { current_line = true },
+  })
+
+  -- Create an autocmd group to clear these settings on cursor move
+  local group = vim.api.nvim_create_augroup("HideLineVirtualDiagnostics", { clear = true })
+  vim.api.nvim_create_autocmd("CursorMoved", {
+    group = group,
+    once = true,          -- so it only runs once
+    callback = function()
+      -- Restore the old diagnostic config
+      vim.diagnostic.config(old_config)
+      -- Delete the group to clean up
+      vim.api.nvim_del_augroup_by_id(group)
+    end,
+  })
+end
+
+vim.keymap.set("n", "L", show_line_virtual_diagnostics, { noremap = true, silent = true })
