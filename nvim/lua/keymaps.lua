892c4462888b128ebe93ff07729419f8613badda
Ashley Fae <ashley.fae.uwu@gmail.com>
2025-02-08 20:09:24 +0100

    🎨 Reformatting

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,25 +1,25 @@
 -- Only show virtual lines on keybind and hide it right after
 local function show_line_virtual_diagnostics()
-  -- Save the current diagnostic settings so we can revert
-  local old_config = vim.deepcopy(vim.diagnostic.config())
+    -- Save the current diagnostic settings so we can revert
+    local old_config = vim.deepcopy(vim.diagnostic.config())
 
-  -- Turn on virtual_lines just for the current line
-  vim.diagnostic.config({
-    virtual_lines = { current_line = true },
-  })
+    -- Turn on virtual_lines just for the current line
+    vim.diagnostic.config({
+        virtual_lines = { current_line = true },
+    })
 
-  -- Create an autocmd group to clear these settings on cursor move
-  local group = vim.api.nvim_create_augroup("HideLineVirtualDiagnostics", { clear = true })
-  vim.api.nvim_create_autocmd("CursorMoved", {
-    group = group,
-    once = true,
-    callback = function()
-      -- Restore the old diagnostic config
-      vim.diagnostic.config(old_config)
-      -- Delete the group to clean up
-      vim.api.nvim_del_augroup_by_id(group)
-    end,
-  })
+    -- Create an autocmd group to clear these settings on cursor move
+    local group = vim.api.nvim_create_augroup("HideLineVirtualDiagnostics", { clear = true })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+        group = group,
+        once = true,
+        callback = function()
+            -- Restore the old diagnostic config
+            vim.diagnostic.config(old_config)
+            -- Delete the group to clean up
+            vim.api.nvim_del_augroup_by_id(group)
+        end,
+    })
 end
 
 local map = vim.keymap.set
@@ -36,4 +36,5 @@ map("n", "<leader>sh", vim.lsp.buf.signature_help, { desc = "Show signature help
 map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Symbol", noremap = true, silent = true })
 map("n", "L", show_line_virtual_diagnostics, { noremap = true, silent = true })
 map("n", "<C-n>", MiniFiles.open, { desc = "Open file explorer", noremap = true, silent = true })
-map("n", "<leader>o", function() Snacks.picker.buffers({ focus = "list" }) end, { desc = "Browse open buffers", noremap = true, silent = true })
+map("n", "<leader>o", function() Snacks.picker.buffers({ focus = "list" }) end,
+    { desc = "Browse open buffers", noremap = true, silent = true })
