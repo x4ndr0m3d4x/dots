bea4cba2ecdae1992adffc3bdcf4defafaed8f45
Tomáš Grafek <grafetom@fit.cvut.cz>
2025-02-08 18:17:36 +0100

    ✨ Added blink.cmp capabilities to LSPs

--- nvim/lua/plugins/mason_lspconfig.lua
+++ nvim/lua/plugins/mason_lspconfig.lua
@@ -7,10 +7,14 @@ return {
             ensure_installed = { "clangd", "tailwindcss", "html", "ts_ls", "lua_ls", "pyright", "rust_analyzer", "tinymist", "svelte" }
         })
 
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+
         -- Default LSP configuration
         require("mason-lspconfig").setup_handlers {
             function(server_name)
-                require("lspconfig")[server_name].setup {}
+                require("lspconfig")[server_name].setup({
+                    capabilities = capabilities
+                })
             end
         }
     end,
