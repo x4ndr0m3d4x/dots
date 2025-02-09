ee7461e9dde261c4222b2140ac45c63f633d6939
Ashley Fae <ashley.fae.uwu@gmail.com>
2025-02-09 20:14:43 +0100

    ✨ Added debugging and QoL features

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -2,7 +2,7 @@ return {
     "echasnovski/mini.nvim",
     version = false,
     config = function()
-        require("mini.git").setup() -- Required for mini.statusline
+        require("mini.git").setup()  -- Required for mini.statusline
         require("mini.diff").setup({ -- Required for mini.statusline
             view = {
                 style = 'sign',
@@ -14,6 +14,24 @@ return {
         })
         require("mini.statusline").setup()
         require("mini.icons").setup()
-        require("mini.files").setup()
+        require("mini.files").setup()       -- File explorer
+        require("mini.indentscope").setup({ -- Visualize current scope
+            draw = {                        -- Do not animate and display the line immediately
+                delay = 0,
+                animation = require("mini.indentscope").gen_animation.none()
+            },
+            symbol = "│"
+        })
+        require("mini.comment").setup({ -- Autocomment line and selection
+            mappings = {
+                comment_line = "<leader>/",
+                comment_visual = "<leader>/"
+            }
+        })
+        require("mini.pairs").setup()    -- Autopairs
+        require("mini.ai").setup()       -- Selection around/inside
+        require("mini.move").setup()     -- Move line and selection
+        require("mini.surround").setup() -- Surround textobjects
+        require("mini.notify").setup()   -- Notifications
     end
 }
--- nvim/lua/plugins/nvim_lspconfig.lua
+++ nvim/lua/plugins/nvim_lspconfig.lua
@@ -6,6 +6,7 @@ return {
             ft = "lua", -- only load on lua files
             opts = {
                 library = {
+                    "nvim-dap-ui",
                     -- See the configuration section for more details
                     -- Load luvit types when the `vim.uv` word is found
                     { path = "${3rd}/luv/library", words = { "vim%.uv" } },
