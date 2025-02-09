416ee4e1f2e3aed4abb862bafa2afc9ef0e9711b
Ashley Fae <ashley.fae.uwu@gmail.com>
2025-02-09 23:14:03 +0100

    ✨ Inlay hints

--- nvim/lua/plugins/nvim_lspconfig.lua
+++ nvim/lua/plugins/nvim_lspconfig.lua
@@ -9,9 +9,9 @@ return {
                     "nvim-dap-ui",
                     -- See the configuration section for more details
                     -- Load luvit types when the `vim.uv` word is found
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
-            },
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } }
+                }
+            }
         }
     },
     config = function()
