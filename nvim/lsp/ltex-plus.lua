7e2765edb0aae2bc27d738ea15102d5aa7ed5b34
Tomáš Grafek <grafetom@fit.cvut.cz>
2025-03-15 18:21:44 +0100

    ✨ Replaced ltex-plus with harper-ls

--- nvim/lsp/ltex-plus.lua
+++
@@ -1,7 +0,0 @@
--- Download manually and add to $PATH
----@type vim.lsp.Config
-return {
-    cmd = { "ltex-ls-plus" },
-    filetypes = { "gitcommit", "html", "markdown", "plaintext", "tex", "text", "typst" },
-    root_markers = { ".git" }
-}
