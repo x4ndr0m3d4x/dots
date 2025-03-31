e6af76b4b31a90a82f3964d1ea3d76ed913c0241
Tomáš Grafek <grafetom@fit.cvut.cz>
2025-03-31 15:23:41 +0200

    🎨 Prettier blink.cmp

--- nvim/lua/plugins/blink_cmp.lua
+++ nvim/lua/plugins/blink_cmp.lua
@@ -1,7 +1,14 @@
 return {
-    'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
-    version = '*',
+    "saghen/blink.cmp",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+        {
+            "Kaiser-Yang/blink-cmp-dictionary",
+            dependencies = { "nvim-lua/plenary.nvim" }
+        }
+
+    },
+    version = "*",
     opts = {
         keymap = { preset = 'default' },
         appearance = {
@@ -10,9 +17,38 @@ return {
         },
         signature = { enabled = true },
         completion = {
+            keyword = {
+                range = "full"
+            },
+            list = {
+                selection = {
+                    preselect = true,
+                    auto_insert = false
+                }
+            },
             documentation = {
                 auto_show = true
+            },
+            menu = {
+                draw = {
+                    gap = 1,
+                    columns = { { "kind_icon" }, { "label" }, { "kind" } }
+                }
+            }
+        },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer", "dictionary" },
+            providers = {
+                dictionary = {
+                    module = "blink-cmp-dictionary",
+                    name = "Dict",
+                    min_keyword_length = 3,
+                    opts = {
+                        dictionary_directories = { vim.fn.expand("~/.config/nvim/dictionary") }
+                    }
+                }
             }
         }
-    }
+    },
+    opts_extend = { "sources.default" }
 }
