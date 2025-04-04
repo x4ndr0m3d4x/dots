cf8ff61348003ec469a706877ecfedc658ca9d83
Ashley Fae <ashley.fae.uwu@gmail.com>
2025-04-04 23:38:27 +0200

    💄 Moved signature_help keybind over to the plugin file

--- nvim/lua/plugins/blink_cmp.lua
+++ nvim/lua/plugins/blink_cmp.lua
@@ -10,7 +10,10 @@ return {
     },
     version = "*",
     opts = {
-        keymap = { preset = 'default' },
+        keymap = {
+            preset = 'default',
+            ["<S-k>"] = { "show_signature", "hide_signature", "fallback" }
+        },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono'
