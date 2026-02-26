2e22d69de75222974cea5fe78cd07e53fe0b4f61
x4ndr0m3d4x <ashley.fae.uwu@gmail.com>
2026-02-26 19:06:09 +0100

    ✨ Added auto-parentheses, modified conflicting keymap

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -7,6 +7,11 @@ lint.linters_by_ft = {
     typst = { 'vale' }
 }
 
+lint.linters.vale = require("lint.util").wrap(lint.linters.vale, function(diagnostic)
+    diagnostic.severity = vim.diagnostic.severity.HINT
+    return diagnostic
+end)
+
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
         -- try_lint without arguments runs the linters defined in `linters_by_ft`
